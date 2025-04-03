8290be5476bcde1e1dc0c87a4402ee55f11f6ef4
grace <115532189+ph503309@users.noreply.github.com>
2025-04-03 17:17:15 +0800

    Update 06-Immutable.sol

--- preparation/02-Solidity经典用例/06-Immutable.sol
+++ preparation/02-Solidity经典用例/06-Immutable.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract Immutable {
+    // immutable 关键字用于声明不可变变量。与 constant 不同，immutable 变量可以在声明时不进行初始化，但必须在合约的构造函数中完成初始化。一旦初始化完成，其值在合约的生命周期内也不能被修改。immutable 变量会占用存储（storage）空间，但只在部署合约时写入一次值，后续不能再修改。
+    address public immutable MY_ADDRESS;
+    uint256 public immutable MY_UINT;
+
+    constructor(uint256 _myUint) {
+        MY_ADDRESS = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
