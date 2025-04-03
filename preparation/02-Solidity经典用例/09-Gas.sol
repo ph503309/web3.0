d551a4ccd9e1387e7f3d4c65b42064cec38d42a1
grace <115532189+ph503309@users.noreply.github.com>
2025-04-03 17:28:56 +0800

    Update 09-Gas.sol

--- preparation/02-Solidity经典用例/09-Gas.sol
+++ preparation/02-Solidity经典用例/09-Gas.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract Gas {
+    uint256 public i = 0;
+
+    // gas使用完后后导致交易失败。
+    // 状态更改将撤消
+    //已消耗的gas不予退还.
+    function forever() public {
+        // 所以gas循环消耗完
+        // 交易失败
+        while (true) {
+            i += 1;
+        }
+    }
+}
