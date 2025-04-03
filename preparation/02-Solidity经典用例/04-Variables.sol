0e3d2c90708ff204fa1b36aa97b444a0893c15b1
grace <115532189+ph503309@users.noreply.github.com>
2025-04-03 17:07:40 +0800

    Update 04-Variables.sol

--- preparation/02-Solidity经典用例/04-Variables.sol
+++ preparation/02-Solidity经典用例/04-Variables.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract Variables {
+    // 全局变量，存储在 storage 中
+    string public text = "Hello";
+    uint256 public num = 123;
+
+    function doSomething() public {
+        // 局部变量，存储在 memory 中
+        uint256 i = 456;
+
+        uint256 timestamp = block.timestamp; // Current block timestamp
+        address sender = msg.sender; // address of the caller
+    }
+}
