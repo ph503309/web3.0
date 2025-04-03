5975edd0fc943d3efb2c753e0aa87e5bb3df6d4a
grace <115532189+ph503309@users.noreply.github.com>
2025-04-03 17:21:52 +0800

    Update 07-SimpleStorage.sol

--- preparation/02-Solidity经典用例/07-SimpleStorage.sol
+++ preparation/02-Solidity经典用例/07-SimpleStorage.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract SimpleStorage {
+    // 用于存储数字的状态变量,全局变量,存储于 storage
+    uint256 public num;
+
+    // 修改变量值
+    function set(uint256 _num) public {
+        num = _num;
+    }
+
+    // 读取值，默认0
+    function get() public view returns (uint256) {
+        return num;
+    }
+}
