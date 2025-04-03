e4356eefdd9f8ba9f0fcec4d7b2b281645fd43e9
grace <115532189+ph503309@users.noreply.github.com>
2025-04-03 16:52:35 +0800

    Update 02-First-App.sol

--- preparation/02-Solidity经典用例/02-First-App.sol
+++ preparation/02-Solidity经典用例/02-First-App.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract Counter {
+    uint256 public count;
+
+    constructor(uint256 _count){
+        count = _count;
+    }
+
+    // Function to get the current count
+    function get() public view returns (uint256) {
+        return count;
+    }
+
+    // Function to increment count by 1
+    function increment() public {
+        count += 1;
+    }
+
+    // Function to decrement count by 1
+    function decrement() public {
+        // This function will fail if count = 0
+        count -= 1;
+    }
+}
